a28ccd8cf86310c0ebeca8a68880cad367e46bbe
Marc Saliba <2235899@bdeb.qc.ca>
2024-02-07 11:07:21 -0500

    Création de l'environnement pour faire les pages webs
    Faites avec Jad Aldilli.

---
+++ server.js
@@ -0,0 +1,59 @@
+/*
+    Importation des modules requis
+*/
+import express from "express";
+import session from "express-session";
+import path from "path";
+import { fileURLToPath } from "url";
+import mysql from "mysql";
+import { body, validationResult } from "express-validator";
+import dateFormat from "dateformat";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/*
+    Connect to server
+*/
+const server = app.listen(4000, function () {
+    console.log("serveur fonctionne sur 4000... ! ");
+});
+/*
+    Configuration de EJS
+*/
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+/*
+    Importation de Bootstrap
+*/
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+/*
+    Connection au server MySQL
+*/
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "scott",
+    password: "oracle",
+    database: "mybd"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("connected!");
+});
+/*
+    Description des routes
+*/
+app.get("/", function (req, res) {
+    con.query("SELECT * FROM e_events ORDER BY e_start_date DESC", function (err, result) {
+        if (err) throw err;
+        res.render("pages/index.ejs", {
+            siteTitle: "Application simple",
+            pageTitle: "Dealership Rubious",
+            items: result
+        });
+    });
+});
+
