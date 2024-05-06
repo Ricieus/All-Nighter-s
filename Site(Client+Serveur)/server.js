20c0991ba7839beaf141643611cad90e54a363d0
Jad427 <2255199@bdeb.qc.ca>
2024-05-06 11:12:33 -0400

    Update server.js

--- Site(Client+Serveur)/server.js
+++ Site(Client+Serveur)/server.js
@@ -582,22 +582,22 @@ app.get('/pages/administrateur', (req, res) => {
         });
     });
 });
-// app.post('/updateProduct', (req, res) => {
-//     let marque = req.body.marque;
-//     let modele = req.body.modele;
-//     let prix = req.body.prix;
-//     let annee = req.body.annee;
+app.post('/updateProduct', (req, res) => {
+    let marque = req.body.marque;
+    let modele = req.body.modele;
+    let prix = req.body.prix;
+    let annee = req.body.annee;
    
   
-//     const query = `UPDATE voitures SET marque = ?, modele = ?, prix = ?, annee = ? WHERE id_voiture = 1`;
-//     con.query(query, [marque, modele, prix, annee], (error, results) => {
-//       if (error) {
-//         console.error('Erreur lors de la mise à jour du produit:', error);
-//         return res.status(500).json({ error: 'Erreur serveur lors de la mise à jour du produit' });
-//       }
-//       res.redirect('/pages/administrateur');
-// });
-//   });
+    const query = `UPDATE voitures SET marque = ?, modele = ?, prix = ?, annee = ? WHERE id_voiture = 1`;
+    con.query(query, [marque, modele, prix, annee], (error, results) => {
+      if (error) {
+        console.error('Erreur lors de la mise à jour du produit:', error);
+        return res.status(500).json({ error: 'Erreur serveur lors de la mise à jour du produit' });
+      }
+      res.redirect('/pages/administrateur');
+});
+  });
 
 app.post('/command', (req, res) => {
     let uri = process.env.DB_URI;
