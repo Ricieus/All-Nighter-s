934a64f7195f3a9157443804a6dfa59743d1b549
Jad427 <2255199@bdeb.qc.ca>
2024-05-07 20:03:47 -0400

    update voiture

--- Site(Client+Serveur)/server.js
+++ Site(Client+Serveur)/server.js
@@ -582,15 +582,16 @@ app.get('/pages/administrateur', (req, res) => {
         });
     });
 });
-app.post('/updateProduct', (req, res) => {
+app.post('/updateProduct/:id', (req, res) => {
     let marque = req.body.marque;
     let modele = req.body.modele;
     let prix = req.body.prix;
     let annee = req.body.annee;
+    const idVoiture = req.params.id;
+    console.log(idVoiture);
 
-
-    const query = `UPDATE voitures SET marque = ?, modele = ?, prix = ?, annee = ? WHERE id_voiture = 1`;
-    con.query(query, [marque, modele, prix, annee], (error, results) => {
+    const query = `UPDATE voitures SET marque = ?, modele = ?, prix = ?, annee = ? WHERE id_voiture = ?`;
+    con.query(query, [marque, modele, prix, annee, idVoiture], (error, results) => {
         if (error) {
             console.error('Erreur lors de la mise à jour du produit:', error);
             return res.status(500).json({ error: 'Erreur serveur lors de la mise à jour du produit' });
